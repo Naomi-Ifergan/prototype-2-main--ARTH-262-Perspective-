b4a8c1ad8d1eaabc96c3596ac9cb060bf0410eba
Naomi-Ifergan <55000614+Naomi-Ifergan@users.noreply.github.com>
2022-05-16 19:56:36 -0400

    Initial commit

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody rb;
+
+    public float forwardForce = 2000f;
+    public float sidewaysForce = 500f;
+    public float backwardForce = 2000f;
+  
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        //this allows the cube to have the same speed on 
+        //all computers regardless of how many frames per second.  
+        rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+       
+        if ( Input.GetKey("left"))
+        {
+            rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0);
+        }
+
+        if (Input.GetKey("right"))
+        {
+            rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0);
+        }
+        if (Input.GetKey("down"))
+        {
+            rb.AddForce(-backwardForce * Time.deltaTime, 0, 0);
+        }
+    }
+}
