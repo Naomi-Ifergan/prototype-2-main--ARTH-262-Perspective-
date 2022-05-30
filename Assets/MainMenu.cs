600244715676c8ff5ff1e1a32a22991321122cc2
Naomi-Ifergan <55000614+Naomi-Ifergan@users.noreply.github.com>
2022-05-30 17:09:20 -0400

    Minor fixes to the player's material

--- Assets/MainMenu.cs
+++ Assets/MainMenu.cs
@@ -10,6 +10,9 @@ public class MainMenu : MonoBehaviour
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
+    public void ResetGame()
+    {
+        SceneManager.LoadScene(0);
+    }
 
- 
 }
